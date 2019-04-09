04fecf35a402ea794473305823f8002bc1b6cc5e
Marc Walker <37228664+MarcMWalker@users.noreply.github.com>
2019-04-09 15:41:48 +0100

    Game ver1.2
    Removed unused js files that didn't work with isometric plugin, character and camera movement added + jumping. Player level restrictions present but still needs tweaking for more accuracy.

--- Isometric Web Game/js/states/play.js
+++ Isometric Web Game/js/states/play.js
@@ -6,23 +6,25 @@ BasicGame.Boot = function (game) { };
 
 var isoGroup, player;
 
-BasicGame.Boot.prototype =
-{
+BasicGame.Boot.prototype = {
     preload: function () {
         game.load.image('cube', 'assets/images/cube3.png');
+        game.load.image('cube1', 'assets/images/cube2.png');
+        game.load.image('cube2', 'assets/images/cube4.png');
+        game.load.image('cube3', 'assets/images/cube1.png');
 
-        //game.time.advancedTiming = true;
+        game.time.advancedTiming = true;
 
         // Add and enable the plug-in.
         game.plugins.add(new Phaser.Plugin.Isometric(game));
         
-        game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
+        game.world.setBounds(0,0,4450,4450);
         
-        game.world.setBounds(0,0,2255,2255);
+        game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
 
         // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
         // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
-        game.iso.anchor.setTo(0.3, 0.0);
+        game.iso.anchor.setTo(0.5);
 
                 
     },
@@ -30,27 +32,28 @@ BasicGame.Boot.prototype =
         // Create a group for our tiles, so we can use Group.sort
         isoGroup = game.add.group();
         
-        //game.physics.isoarcade.gravity.setTo(0,0,-500);
+        game.physics.isoArcade.gravity.setTo(0,0,-500);
 
         // Let's make a load of cubes on a grid, but do it back-to-front so they get added out of order.
         var map;
-        for (var xx = 0; xx < 2255; xx += 102) {
-            for (var yy = 0; yy < 2255; yy +=102) {
+        for (var xx = 128; xx < 2255; xx += 102) {
+            for (var yy = 128; yy < 2255; yy +=102) {
                 // Create a cube using the new game.add.isoSprite factory method at the specified position.
                 // The last parameter is the group you want to add it to (just like game.add.sprite)
                 map = game.add.isoSprite(xx, yy, 0, 'cube', 0, isoGroup);
-                map.anchor.set(0.5)
+                map.anchor.set(0.5, 0.5);
+                //game.physics.isoArcade.enable(map);
 
                 // Add a slightly different tween to each cube so we can see the depth sorting working more easily.
                 //game.add.tween(cube).to({ isoZ: 10 }, 100 * ((xx + yy) % 10), Phaser.Easing.Quadratic.InOut, true, 0, Infinity, true);
             }
         }
         
-        player = game.add.isoSprite(128,128,0,'cube',0,isoGroup);
+        player = game.add.isoSprite(128,128,0,'cube1',0,isoGroup);
         player.tint = 0x86bfda;
         player.anchor.set(0.5);
-        //game.physics.isoArcade.enable(player);
-        //player.body.collideWorldBounds = true;
+        game.physics.isoArcade.enable(player);
+        player.body.collideWorldBounds = true;
         
         var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
             
@@ -71,7 +74,7 @@ BasicGame.Boot.prototype =
         ])
     },
     update: function () {
-        var speed = 100;
+        var speed = 400;
         
         if(this.cursors.up.isDown){
             player.body.velocity.y = -speed;
@@ -80,8 +83,19 @@ BasicGame.Boot.prototype =
             player.body.velocity.y = speed;
         }
         else {
-            //player.body.velocity.y = 0;
+            player.body.velocity.y = 0;
         }
+        
+        if(this.cursors.left.isDown){
+            player.body.velocity.x = -speed;
+        }
+        else if(this.cursors.right.isDown){
+            player.body.velocity.x = speed;
+        }
+        else{
+            player.body.velocity.x = 0;
+        }
+        
     },
     render: function () {
     }
