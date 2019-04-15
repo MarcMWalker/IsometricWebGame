7850fe48db30659fc647b3a519e83c894147799d
lcdavison <luke.davison@talktalk.net>
2019-04-15 17:46:18 +0100

    Adjusted speed to fit animation

--- js/states/play.js
+++ js/states/play.js
@@ -86,21 +86,14 @@ BasicGame.Boot.prototype = {
         player.tint = 0x86bfda;
         player.anchor.set ( 0.5, 0.5 );
         
-        player.animations.add('S', [79,88,89,97,98,99,106,107,108,109,115,116,117,118,119],6,true);
-        player.animations.add('N', [19,28,29,37,38,39,46,47,48,49,55,56,57,58,59],6,true);
+        player.animations.add('S', [79,88,89,97,98,99,106,107,108,109,115,116,117,118,119], 6,true);
+        player.animations.add('N', [19,28,29,37,38,39,46,47,48,49,55,56,57,58,59], 6,true);
         player.animations.add('W', [65,66,74,75,83,84,92,93,94,101,102,103,110,111,112],6,true);
-         player.animations.add('E', [30,31,32,33,34,40,41,42,43,44,50,51,52,53,54],6,true);
+        player.animations.add('E', [30,31,32,33,34,40,41,42,43,44,50,51,52,53,54],6,true);
         player.animations.add('NE',5,6,7,8,9,15,16,17,18,25,26,27,35,36,45);
-        
         
         game.physics.isoArcade.enable(player);
         player.body.collideWorldBounds = true;
-        
-        var space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-            
-        space.onDown.add(function() {
-            player.body.velocity.z = 300;                 
-        }, this);
         
         game.camera.follow(player);
         
@@ -115,7 +108,7 @@ BasicGame.Boot.prototype = {
         ])
     },
     update: function () {
-        var speed = 1000;
+        var speed = 400;
         
         if(this.cursors.up.isDown){
             player.body.velocity.y = -speed;
@@ -140,10 +133,7 @@ BasicGame.Boot.prototype = {
             player.animations.stop();
         }
         
-
-        //console.log ( "ISO X : " + player.isoX + " ISO Y : " + player.isoY );
-        
-    game.physics.isoArcade.collide(isoGroup2);
+    	game.physics.isoArcade.collide(isoGroup2);
         game.iso.topologicalSort(isoGroup2);
         
         //Adding feature to destroy enemies here just by touching them at the moment
