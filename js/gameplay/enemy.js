81cd6eae700be91dcba3b786a75dc5a9a7def38a
lcdavison <luke.davison@talktalk.net>
2019-04-16 14:30:23 +0100

    Calculate direction vector to player

--- js/gameplay/enemy.js
+++ js/gameplay/enemy.js
@@ -1,10 +1,26 @@
 class Enemy 
 {
+	//	TODO:	Add a line for the enemy to follow, to imitate patrolling
+	//	TODO:	When player is close to enemy, enemy will attack the player
+	
 	/*
 	 *	Construct enemy at position
 	 * */
 	constructor ( position )
 	{
-		this.sprite = game.add.isoSprite ( position.x, position.y, 0, "cube2" );
+		this.sprite = game.add.isoSprite ( position.x, position.y, 0, "cube2", 0, isoGroup2 );
+	}
+
+	position ( )
+	{
+		return new Vector2 ( this.sprite.x, this.sprite.y );
+	}
+
+	update ( )
+	{
+		//	Get Player position
+		var distance = this.position ( ).sub ( player.position ( ) );
+
+		//	Calculate distance to player
 	}
 }
