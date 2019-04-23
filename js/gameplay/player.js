62aa08ce48d38a0d3664e04852692e0bca46c2b4
lcdavison <luke.davison@talktalk.net>
2019-04-23 12:23:01 +0100

    Fixed NE animation

--- js/gameplay/player.js
+++ js/gameplay/player.js
@@ -11,7 +11,7 @@ class Player
 		this.sprite.animations.add('N', [19,28,29,37,38,39,46,47,48,49,55,56,57,58,59], 6,true);
 		this.sprite.animations.add('W', [65,66,74,75,83,84,92,93,94,101,102,103,110,111,112],6,true);
 		this.sprite.animations.add('E', [30,31,32,33,34,40,41,42,43,44,50,51,52,53,54],6,true);
-		this.sprite.animations.add('NE',5,6,7,8,9,15,16,17,18,25,26,27,35,36,45);
+		this.sprite.animations.add('NE', [5,6,7,8,9,15,16,17,18,25,26,27,35,36,45], 6, true);
 
 		game.physics.isoArcade.enable ( this.sprite );
 		this.sprite.body.collideWorldBounds = true;
