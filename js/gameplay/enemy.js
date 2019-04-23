6c0c3f74107161cbbbb197b7f0843444bc9a010f
lcdavison <luke.davison@talktalk.net>
2019-04-23 09:31:40 +0100

    Added enemy movement towards player

--- js/gameplay/enemy.js
+++ js/gameplay/enemy.js
@@ -2,7 +2,7 @@ class Enemy
 {
 	//	TODO:	Add a line for the enemy to follow, to imitate patrolling
 	//	TODO:	When player is close to enemy, enemy will attack the player
-	
+
 	/*
 	 *	Construct enemy at position
 	 * */
@@ -13,14 +13,28 @@ class Enemy
 
 	get position ( )
 	{
-		return new Vector2 ( this.sprite.x, this.sprite.y );
+		return new Vector2 ( this.sprite.isoX, this.sprite.isoY );
+	}
+
+	lerp ( start, end, alpha )
+	{
+		return ( 1.0 - alpha ) * start + alpha * end;
 	}
 
 	update ( )
 	{
 		//	Get Player position
-		var distance = this.position.sub ( player.position );
+		var distance = player.position.sub ( this.position );
 
-		//	Calculate distance to player
+		//	Get Distance Squared
+		var distSqr = distance.dot ( distance );
+
+		if ( distSqr < 100000 )
+		{
+			this.sprite.isoX = this.lerp ( this.position.x, player.position.x, 0.001 );
+			this.sprite.isoY = this.lerp ( this.position.y, player.position.y, 0.001 );
+		}
+
+		//	Attack When Close
 	}
 }
