a4f1231200df615823e5b5a902e4af936515d832
lcdavison <luke.davison@talktalk.net>
2019-04-26 12:03:08 +0100

    Adjusted animation frame rate for smoother animations

--- js/gameplay/enemy.js
+++ js/gameplay/enemy.js
@@ -11,14 +11,14 @@ class Enemy
 
 		this.sprite.tint = 0xFF0000;
 
-		this.sprite.animations.add('S', [21,43,65,87,109,131,153,175,197,219,241,43], 6,true);
-		this.sprite.animations.add('N', [484,485,486,487,488,489,490,491,492,493,494,495,496,497], 6,true);
-		this.sprite.animations.add('W', [514,515,516,517,518,519,520,521,522,523,524,525,526,528,529],6,true);
-		this.sprite.animations.add('E', [451,452,453,454,455,456,457,458,459,462,463,464,465],6,true);
-		this.sprite.animations.add('NE', [467,468,469,470,471,472,473,474,475,476,477,478,479,480,481], 6, true);
-		this.sprite.animations.add('SE', [435,436,437,438,441,442,443,445,446,447,448,449,450], 6, true);
-		this.sprite.animations.add('SW', [530,531,532,533,534,535,536,537,538,539,540,541,542,543,544], 6, true);
-		this.sprite.animations.add('NW', [498,499,500,501,502,503,504,506,507,508,509,510,511,512,513], 6, true);
+		this.sprite.animations.add('S', [21,43,65,87,109,131,153,175,197,219,241,43], 15,true);
+		this.sprite.animations.add('N', [484,485,486,487,488,489,490,491,492,493,494,495,496,497], 15,true);
+		this.sprite.animations.add('W', [514,515,516,517,518,519,520,521,522,523,524,525,526,528,529], 15,true);
+		this.sprite.animations.add('E', [451,452,453,454,455,456,457,458,459,462,463,464,465], 15,true);
+		this.sprite.animations.add('NE', [467,468,469,470,471,472,473,474,475,476,477,478,479,480,481], 15, true);
+		this.sprite.animations.add('SE', [435,436,437,438,441,442,443,445,446,447,448,449,450], 15, true);
+		this.sprite.animations.add('SW', [530,531,532,533,534,535,536,537,538,539,540,541,542,543,544], 15, true);
+		this.sprite.animations.add('NW', [498,499,500,501,502,503,504,506,507,508,509,510,511,512,513], 15, true);
 
 		this.intAnimationMask = 0;
 	}
--- js/gameplay/player.js
+++ js/gameplay/player.js
@@ -8,24 +8,24 @@ class Player
 		this.sprite.tint = 0x86bfda;
 		this.sprite.anchor.set ( 0.5, 0.5 );
 
-		this.sprite.animations.add('S', [79,88,89,97,98,99,106,107,108,109,115,116,117,118,119], 6,true);
-		this.sprite.animations.add('N', [19,28,29,37,38,39,46,47,48,49,55,56,57,58,59], 6,true);
-		this.sprite.animations.add('W', [65,66,74,75,83,84,92,93,94,101,102,103,110,111,112],6,true);
-		this.sprite.animations.add('E', [30,31,32,33,34,40,41,42,43,44,50,51,52,53,54],6,true);
-		this.sprite.animations.add('NE', [5,6,7,8,9,15,16,17,18,25,26,27,35,36,45], 6, true);
-        	this.sprite.animations.add('SE', [0,1,2,3,4,10,11,12,13,14,20,21,22,23,24], 6, true);
-        	this.sprite.animations.add('SW', [67,68,69,76,77,78,85,86,87,95,96,104,105,113,114], 6, true);
-        	this.sprite.animations.add('NW', [60,61,62,63,64,70,71,72,73,80,81,82,90,91,100], 6, true);
+		/*this.sprite.animations.add('S', [79,88,89,97,98,99,106,107,108,109,115,116,117,118,119], 15,true);
+		this.sprite.animations.add('N', [19,28,29,37,38,39,46,47,48,49,55,56,57,58,59], 15,true);
+		this.sprite.animations.add('W', [65,66,74,75,83,84,92,93,94,101,102,103,110,111,112],15,true);
+		this.sprite.animations.add('E', [30,31,32,33,34,40,41,42,43,44,50,51,52,53,54], 15,true);
+		this.sprite.animations.add('NE', [5,6,7,8,9,15,16,17,18,25,26,27,35,36,45], 15, true);
+        	this.sprite.animations.add('SE', [0,1,2,3,4,10,11,12,13,14,20,21,22,23,24], 15, true);
+        	this.sprite.animations.add('SW', [67,68,69,76,77,78,85,86,87,95,96,104,105,113,114], 15, true);
+        	this.sprite.animations.add('NW', [60,61,62,63,64,70,71,72,73,80,81,82,90,91,100], 15, true);*/
         
 		//Movement animations
-		this.sprite.animations.add('S', [21,43,65,87,109,131,153,175,197,219,241,43], 6,true);
-		this.sprite.animations.add('N', [484,485,486,487,488,489,490,491,492,493,494,495,496,497], 6,true);
-		this.sprite.animations.add('W', [514,515,516,517,518,519,520,521,522,523,524,525,526,528,529],6,true);
-		this.sprite.animations.add('E', [451,452,453,454,455,456,457,458,459,462,463,464,465],6,true);
-		this.sprite.animations.add('NE', [467,468,469,470,471,472,473,474,475,476,477,478,479,480,481], 6, true);
-		this.sprite.animations.add('SE', [435,436,437,438,441,442,443,445,446,447,448,449,450], 6, true);
-		this.sprite.animations.add('SW', [530,531,532,533,534,535,536,537,538,539,540,541,542,543,544], 6, true);
-		this.sprite.animations.add('NW', [498,499,500,501,502,503,504,506,507,508,509,510,511,512,513], 6, true);
+		this.sprite.animations.add('S', [21,43,65,87,109,131,153,175,197,219,241,43], 15,true);
+		this.sprite.animations.add('N', [484,485,486,487,488,489,490,491,492,493,494,495,496,497], 15,true);
+		this.sprite.animations.add('W', [514,515,516,517,518,519,520,521,522,523,524,525,526,528,529], 15,true);
+		this.sprite.animations.add('E', [451,452,453,454,455,456,457,458,459,462,463,464,465], 15,true);
+		this.sprite.animations.add('NE', [467,468,469,470,471,472,473,474,475,476,477,478,479,480,481], 15, true);
+		this.sprite.animations.add('SE', [435,436,437,438,441,442,443,445,446,447,448,449,450], 15, true);
+		this.sprite.animations.add('SW', [530,531,532,533,534,535,536,537,538,539,540,541,542,543,544], 15, true);
+		this.sprite.animations.add('NW', [498,499,500,501,502,503,504,506,507,508,509,510,511,512,513], 15, true);
 
 		//Attack animations
 		//this.sprite.animations.add('AS', [132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149], 6,true);
