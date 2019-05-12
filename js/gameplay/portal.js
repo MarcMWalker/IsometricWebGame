60d4c9975562a128890ad3b69941adb38e31e133
lcdavison <luke.davison@talktalk.net>
2019-05-12 23:49:41 +0100

    Fixed animation

--- js/gameplay/portal.js
+++ js/gameplay/portal.js
@@ -3,12 +3,15 @@ class Portal
 	constructor ( position )
 	{
 		//Portal sprite added to game as isoSprite, given portal animation and added to isoGroup = portalGroup
-		this.sprite = game.add.isoSprite (position.x, position.y, 0, 'portal', portalGroup );
+		this.sprite = game.add.isoSprite ( position.x, position.y, 0, 'portal', portalGroup );
+		this.sprite.visible = false;
 		this.sprite.anchor.set ( 0.5, 0.5 );
 
 		//Portal animations for activation and repeating once active
-		this.sprite.animations.add('activate', [1,2,3,4,5,7,8,9,10,11,12,14,15,16,17,18,19,21,22,23,24,25,26, 6,13,20,27,28,29], 15, true );
-		this.sprite.animations.add('repeat', [6,13,20,27,28,29], 13, true );
+		this.sprite.animations.add ( 'activate', [1,2,3,4,5,7,8,9,10,11,12,14,15,16,17,18,19,21,22,23,24,25,26, 6,13,20,27,28,29], 15 );
+		this.sprite.animations.add ( 'repeat', [6,13,20,27,28,29], 6, true );
+
+		this.bolActive = false;
 	}
 
 	//X, Y location for portal sprite
@@ -19,15 +22,24 @@ class Portal
 
 	animate ( )
 	{
-		//	Switch statement to change between animation types
-		switch ( this.intAnimationMask )
+		if ( this.sprite.animations.currentAnim.isFinished )
 		{
-			case 1:
-				this.sprite.animations.play ( 'activate' );
-				break;
-			case 2:
-				this.sprite.animations.play ( 'repeat' );
-				break;		
+			this.bolActive = true;
 		}
+
+		if ( this.bolActive )
+		{
+			this.sprite.animations.play ( 'repeat' );
+		}
+		else 
+		{
+			this.sprite.animations.play ( 'activate' );
+		}
+	}
+
+	reset ( )
+	{
+		this.bolActive = false;
+		this.sprite.visible = true;
 	}
 }
